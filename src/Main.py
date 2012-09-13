497cc3cf9f2f405b43f7e359696246c295893501
Tyler Smith <tylertsmith14@gmail.com>
2012-09-12 22:01:02 -0400

    Commit for HW3

--- src/Main.py
+++ src/Main.py
@@ -7,28 +7,34 @@ import webapp2
 import jinja2
 import os
 import cgi
+from google.appengine.api import users
 
 jinja_environment = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__) + "/templates"))
 
 class MainPage(webapp2.RequestHandler):
-  def get(self):      
-        template_values = {
-                'title': 'Homework 2'
+  def get(self):  
+        user = users.get_current_user()
+        templateValues = {
+                'title': 'Pinboard'
         }
-        template = jinja_environment.get_template('form.html')
-        self.response.out.write(template.render(template_values))
-        
-class Submit(webapp2.RequestHandler):
-    def get(self):
-        template_values = {
-                           'title': 'Homework 2',
-                           'imageURL' : cgi.escape(self.request.get('imageURL')),
-                           'caption' : cgi.escape(self.request.get('caption'))
-                           }
-        template = jinja_environment.get_template('formSubmit.html')
-        self.response.out.write(template.render(template_values))
+        template = jinja_environment.get_template('master.html')
 
-app = webapp2.WSGIApplication([('/', MainPage),
-                              ('/submit',Submit)],
+        if user:
+            templateValues["email"] = user.email()
+            templateValues["logoutUrl"] = users.create_logout_url(self.request.uri)   
+            if self.request.get('imageUrl') != None:
+                templateValues['imageUrl'] = self.request.get('imageUrl')
+            if self.request.get('caption') != None:
+                templateValues['caption'] = self.request.get('caption')
+            template = jinja_environment.get_template('form.html')
+            
+        else:
+            templateValues["loginUrl"] = users.create_login_url(self.request.uri)   
+            template = jinja_environment.get_template('login.html')
+        
+        self.response.out.write(template.render(templateValues))
+        
+
+app = webapp2.WSGIApplication([('/', MainPage)],
                               debug=True)
